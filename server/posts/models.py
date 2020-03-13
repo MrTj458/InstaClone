9afffa7ddc414d14e55f72bd767fdf289355b6c5
MrTj458 <mrtj458@gmail.com>
2020-03-12 20:28:45 -0600

    Initial Post and Comment models

--- server/posts/models.py
+++ server/posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(get_user_model(), models.CASCADE)
+    image = models.ImageField(upload_to='posts')
+    created_at = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    likes = models.ManyToManyField(get_user_model(), related_name='likes')
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, models.CASCADE, related_name='comments')
+    author = models.ForeignKey(
+        get_user_model(), models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
