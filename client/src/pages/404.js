641c8b03d9b93cfdfba157b866545efa6488423f
MrTj458 <mrtj458@gmail.com>
2020-03-20 20:59:14 -0600

    404 page styled

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -1,9 +1,72 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const wiggle = keyframes`
+  0% {
+    transform: translateX(-5px);
+  }
+  50% {
+    transform: translateX(5px);
+  }
+  100% {
+    transform: translateX(-5px);
+  }
+`
+
+const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    font-family: 'Pacifico', cursive;
+    font-size: 56px;
+  }
+
+  h2 {
+    font-family: 'Pacifico', cursive;
+    font-size: 28px;
+  }
+
+  p {
+    margin-bottom: 10px;
+    font-size: 18px;
+  }
+
+  h3 {
+    font-family: 'Pacifico', cursive;
+    font-size: 24px;
+    margin-left: 15px;
+
+    :hover {
+      text-decoration: underline;
+    }
+  }
+`
+
+const BackLink = styled(Link)`
+  text-decoration: none;
+  color: #3897f0;
+  display: flex;
+  align-items: center;
+
+  i {
+    animation: ${wiggle} 1s linear infinite;
+  }
+`
 
 export default function NotFound() {
   return (
-    <>
-      <h1>404! Page not found</h1>
-    </>
+    <NotFoundContainer>
+      <h1>404</h1>
+      <h2>Oh No!</h2>
+      <p>We could not find the page you are looking for.</p>
+      <BackLink style={{ textDecoration: 'none' }} to="/">
+        <i className="fas fa-angle-double-left fa-2x"></i>
+        <h3>Take me home!</h3>
+      </BackLink>
+    </NotFoundContainer>
   )
 }
